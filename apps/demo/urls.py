e8f995e886b4042d33079977935a0ad546fa82e3
amin <amin.r.abromand@gmail.com>
2019-01-13 18:39:38 +0100

    added manifest.json to demo

--- apps/demo/urls.py
+++ apps/demo/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from .views import (
 	demo_slide_show,
 	slide_show_pictures,
+	manifest_json,
 	)
 
 app_name='demo'
@@ -10,5 +11,6 @@ app_name='demo'
 urlpatterns = [
 	url(r'^slideshow/$', demo_slide_show, name='slideshow'),
 	url(r'^images/$', slide_show_pictures, name='images'),
+	url(r'^slideshow/manifest.json$', manifest_json, name='manifest'),
 ]
 
--- apps/demo/views.py
+++ apps/demo/views.py
@@ -20,3 +20,28 @@ def slide_show_pictures(request):
 		images.append(slide.image.url)
 	response = {'images': images}
 	return JsonResponse(response)
+
+def manifest_json(request):
+	response = {
+		  "short_name": "Maps",
+		  "name": "Google Maps",
+		  "icons": [
+		    {
+		      "src": "/static/demo/slideshow/images/icons-240.png",
+		      "type": "image/png",
+		      "sizes": "240x240"
+		    },
+		    {
+		      "src": "/static/demo/slideshow/images/icons-512.png",
+		      "type": "image/png",
+		      "sizes": "512x512"
+		    }
+		  ],
+		  "start_url": "/demo/slideshow/?source=pwa",
+		  "background_color": "#3367D6",
+		  "display": "standalone",
+		  "scope": "/",
+		  "theme_color": "#3367D6"
+		}
+
+	return JsonResponse(response)
